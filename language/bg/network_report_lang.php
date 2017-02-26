2901dd43becc8f52e83c436409930dbbf705fe63
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:35:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Докладът за Network осигурява мрежа пропускателна информация за всички ваши мрежови интерфейси.';
+$lang['network_report_app_name'] = 'Доклад Network';
