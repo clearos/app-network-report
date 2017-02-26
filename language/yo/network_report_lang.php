44746d723fb4ec883d1954664e5f274930fcd6f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:45:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'The Network Iroyin pese nẹtiwọki losi alaye lori gbogbo nẹtiwọki rẹ atọkun.';
+$lang['network_report_app_name'] = 'Network Iroyin';
