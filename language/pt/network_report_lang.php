9e9d582ae066673c6948dc0d59a3ea551d48cac3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:41:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'O Relatório de rede fornece rede de informação taxa de transferência em todas as suas interfaces de rede.';
+$lang['network_report_app_name'] = 'Relatório de rede';
