6b865f38a8e6c69754d8f2ca3a534524eee7537d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:36:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Le Rapport sur le réseau fournit des informations sur le réseau débit sur tous vos interfaces réseau.';
+$lang['network_report_app_name'] = 'Rapport sur le réseau';
