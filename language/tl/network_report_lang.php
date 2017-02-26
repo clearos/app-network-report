fa9e68007424ee53d4bfb9b0617a214e8df95223
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:44:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Ang Network Report nagbibigay network throughput impormasyon sa lahat ng iyong mga interface ng network.';
+$lang['network_report_app_name'] = 'network Report';
