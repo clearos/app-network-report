975f40b55ba39d09afe359bb4a0314fac08136ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:42:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Správa Network poskytuje priepustnosť siete informácie o všetkých vašich sieťových rozhraní.';
+$lang['network_report_app_name'] = 'Report Network';
