572f74a97595b7794dface1b20c094020b1530b2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:35:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'အဆိုပါကွန်ယက်အစီရင်ခံစာအားလုံးသင့် network interface တွေကိုအပေါ်ကွန်ယက်ကို throughput သတင်းအချက်အလက်ပေးစွမ်းသည်။';
+$lang['network_report_app_name'] = 'Network မှအစီရင်ခံစာ';
