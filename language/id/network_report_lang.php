88bfedbaac5acef61397eb808907008b05aabd24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:37:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Jaringan Report memberikan informasi jaringan throughput pada semua antarmuka jaringan Anda.';
+$lang['network_report_app_name'] = 'Laporan jaringan';
