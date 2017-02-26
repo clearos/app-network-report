27d129d6cfe6f3aa144261abb1847646d013ee6b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:45:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Mae Adroddiad y Rhwydwaith yn darparu gwybodaeth trwybwn rhwydwaith ar eich holl rhyngwynebau rhwydwaith.';
+$lang['network_report_app_name'] = 'Adroddiad rhwydwaith';
