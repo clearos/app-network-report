af870318b76ed91d00fe7637b9b251b959132dbd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:36:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'La Reto Raporti provizas reton traigivo informo sur ĉiuj viaj reto interfacoj.';
+$lang['network_report_app_name'] = 'reto Raporti';
