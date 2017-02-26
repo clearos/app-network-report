88b22428e8f67063a1a472769ea46c6a17ea65a3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:45:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Umbiko Network inikeza inethiwekhi ulwazi throughput on wonke inethiwekhi yakho interface.';
+$lang['network_report_app_name'] = 'Umbiko Network';
