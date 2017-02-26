6f083391ede945b906ef9a9771adec4ab6aaf1fc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:36:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Netværket rapporten giver netværkshastighed oplysninger om alle dine netværksgrænseflader.';
+$lang['network_report_app_name'] = 'Netværk rapport';
