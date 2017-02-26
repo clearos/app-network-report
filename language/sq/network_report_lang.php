e4c85cd2fe77a17ed451773023692943203ce058
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:34:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Raporti Network ofron rrjetit informacione të xhiros në të gjitha ndërfaqet e rrjetit tuaj.';
+$lang['network_report_app_name'] = 'Rrjeti Raporti';
