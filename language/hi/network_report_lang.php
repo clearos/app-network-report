ddaa7cb5e6adeea1c2e9060cfd7d6e72d1dc94a5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:37:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'नेटवर्क रिपोर्ट अपने सभी नेटवर्क इंटरफेस पर नेटवर्क throughput जानकारी प्रदान करता है।';
+$lang['network_report_app_name'] = 'नेटवर्क की रिपोर्ट';
