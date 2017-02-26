9e2e188eed1896790f79e7140ce2be0b5c7734bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:38:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Tugann an Tuarascáil Líonra gréasáin faisnéise tréchur ar gach do comhéadan líonra.';
+$lang['network_report_app_name'] = 'Tuarascáil líonra';
