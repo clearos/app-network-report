b16dcbfeca57706f99509315ea2143f05e0cb24a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:39:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Tīkla pārskats sniedz tīkla caurlaides informāciju par visiem jūsu tīkla saskarnes.';
+$lang['network_report_app_name'] = 'tīkla pārskats';
