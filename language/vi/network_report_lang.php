34272242e4cb462bbadb04c82816122a46dbe3bc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:45:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Báo cáo mạng cung cấp mạng thông tin thông trên tất cả các giao diện mạng của bạn.';
+$lang['network_report_app_name'] = 'báo cáo mạng';
