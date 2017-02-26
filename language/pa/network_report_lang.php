11372ce4901f819befadfee8a8b48c7fb3d64281
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:41:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'ਨੈੱਟਵਰਕ ਦੀ ਰਿਪੋਰਟ ਤੁਹਾਡੇ ਸਾਰੇ ਨੈੱਟਵਰਕ ਇੰਟਰਫੇਸ ਉੱਪਰ ਨੈੱਟਵਰਕ ਥਰੋ ਜਾਣਕਾਰੀ ਦਿੰਦਾ ਹੈ.';
+$lang['network_report_app_name'] = 'ਨੈੱਟਵਰਕ ਦੀ ਰਿਪੋਰਟ';
