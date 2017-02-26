6c995a4b5e3ef500d8721433aab35d43f0716625
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:40:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Ny Network Report manome tambajotra throughput vaovao momba ny tambajotra interface rehetra.';
+$lang['network_report_app_name'] = 'Network Report';
