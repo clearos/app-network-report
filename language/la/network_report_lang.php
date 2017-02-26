6331a61566f0b2b98e97db7211730e537bf70f72
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:39:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Report throughput notitia network praebet quod network retiacula in omnibus interfaces.';
+$lang['network_report_app_name'] = 'Network Report';
