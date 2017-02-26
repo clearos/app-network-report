aba9eea3b5d628a47730b8716a7eb1b756b50fc6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:43:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'The Report Network fana ka ya netweke throughput tlhahisoleseding e mabapi le likarolo tsohle tsa hao ya netweke.';
+$lang['network_report_app_name'] = 'Report Network';
