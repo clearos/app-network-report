edb2b0a6ea60d9a55b5530cfa2b8637a01d5b353
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:35:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'The Report Network amapereka Intaneti matulukidwe mudziwe polumikizira anu onse Intaneti.';
+$lang['network_report_app_name'] = 'Report Network';
