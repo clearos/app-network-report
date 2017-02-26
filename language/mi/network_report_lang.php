fca491e0cfb0bc31427407c6d3dea73f2f7735c8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:40:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'whakarato te Report Network whatunga mōhiohio e puta i runga i koutou atanga katoa whatunga.';
+$lang['network_report_app_name'] = 'Pūrongo whatunga';
