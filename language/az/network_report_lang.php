dce21fd314ae7e204aa43709ef04c758fe383555
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:34:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Network Report bütün şəbəkə interfeysləri şəbəkə ötürücülük məlumat verir.';
+$lang['network_report_app_name'] = 'Network Report';
