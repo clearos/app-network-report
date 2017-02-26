0eba8f02b83cdd307603c4b3099c2d9d1ae0d1ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:37:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'The Network Report samar da cibiyar sadarwa kayan da aka samar bayani a kan dukan afaretan cibiyar musaya.';
+$lang['network_report_app_name'] = 'Network Report';
