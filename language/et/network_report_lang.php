e916ff1e3fc2c676ecf2e942bf9d8bc6cb5e012d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:36:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Võrgustik aruanne pakub võrgu läbilaskevõime teavet kõigi oma võrgu liidesed.';
+$lang['network_report_app_name'] = 'võrgu aruanne';
