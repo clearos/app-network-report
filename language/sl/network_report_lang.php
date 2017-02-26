155398931411aa0d038eda8bd5db342d5fd65413
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:42:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Poročilo Mreža zagotavlja omrežno prepustnost informacije o vseh omrežnih vmesnikov.';
+$lang['network_report_app_name'] = 'Poročilo omrežja';
