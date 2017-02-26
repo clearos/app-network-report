d4206d974e1f053426c068ed2c16a1a47f994d20
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:35:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Zpráva Network poskytuje propustnost sítě informace o všech vašich síťových rozhraní.';
+$lang['network_report_app_name'] = 'Report Network';
