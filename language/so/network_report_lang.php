9ff678ce431b7d435b419f457b6ab4e01f1f8834
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:42:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Report Network wuxuu bixiyaa macluumaad network dhex on interfaces network oo dhan.';
+$lang['network_report_app_name'] = 'Network Report';
