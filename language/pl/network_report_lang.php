bea81073fc5bb220a44d947bf87dba655dddc996
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:41:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Raport Network zapewnia przepustowość sieci informacje na temat wszystkich interfejsów sieciowych.';
+$lang['network_report_app_name'] = 'Zgłoś Network';
