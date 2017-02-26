6c28e030e00fa060fbb51a9ac6ce9ea647968fcf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:38:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Il rapporto di rete fornisce informazioni throughput di rete su tutte le interfacce di rete.';
+$lang['network_report_app_name'] = 'rapporto di rete';
