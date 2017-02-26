761d13a86860f69bd958611545e41182a7e25491
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:45:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Tarmoq Hisobot barcha tarmoq interfeysi tarmoq throughput ma&#39;lumot beradi.';
+$lang['network_report_app_name'] = 'tarmoq hisoboti';
