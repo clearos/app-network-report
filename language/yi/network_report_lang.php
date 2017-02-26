55cc5ffd7bfcb6128b8718bb899d91e88d94ad33
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:45:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'די נעץ באריכט גיט נעץ טרופּוט אינפֿאָרמאַציע אויף אַלע אייער נעץ Interfaces.';
+$lang['network_report_app_name'] = 'נעטוואָרק באריכט';
