ea18998dc6ef3c69980c197c1e21c1b5ba9970eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:42:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Мрежа Извештај пружа мреже пропусни информације о свим својим мрежних интерфејса.';
+$lang['network_report_app_name'] = 'Извештај о мрежи';
