9c5987b7b4c48d2e0c6acac20c61a8d7563840d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:43:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Nätverket rapporten ger nätverk genomströmning information om alla dina nätverksgränssnitt.';
+$lang['network_report_app_name'] = 'nätverks~~POS=TRUNC rapport';
