302474ac026916c04451840aaa92b8d22a0d3759
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:40:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Laporan Network menyediakan rangkaian maklumat throughput pada semua antara muka rangkaian anda.';
+$lang['network_report_app_name'] = 'Laporan Network';
