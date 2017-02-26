47aeb03ac033f49badb67e1337c66b3f9311d0a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:34:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Network txostena The sare throughput zure sare interfazeak buruzko informazioa eskaintzen du.';
+$lang['network_report_app_name'] = 'Network txostena';
