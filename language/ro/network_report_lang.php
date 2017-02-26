822b2d65d38cbcc6c64f2e46c6b785b8e7af8aa0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:42:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Raportul de rețea furnizează informații de rețea de transfer pe toate interfețele de rețea.';
+$lang['network_report_app_name'] = 'Raport de rețea';
