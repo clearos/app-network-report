e41e9c61c7cd9c2836beaa96fffc0a7618ba601d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:39:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Tinklo ataskaitoje pateikiama tinklo pralaidumo informacija apie visus savo tinklo sąsajų.';
+$lang['network_report_app_name'] = 'Tinklo ataskaita';
