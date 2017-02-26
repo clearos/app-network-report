48dd64b3579c66449067255aa1a7bcec9e19ca33
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:40:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'नेटवर्क अहवाल आपल्या सर्व नेटवर्क संवाद नेटवर्क प्रक्रिया करून माहिती पुरवतो.';
+$lang['network_report_app_name'] = 'नेटवर्क अहवाल';
