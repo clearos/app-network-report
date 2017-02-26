45ac40e8a664ccf44328f6f7518b11cec8875b89
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:34:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Ցանցը Զեկուցել տրամադրում ցանցային տեղեկատվություն բոլոր ձեր ցանցային ինտերֆեյս.';
+$lang['network_report_app_name'] = 'Ցանցի հաշվետվություն';
