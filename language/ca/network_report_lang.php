19fa6aa4132337eaa6c987607d660060b04139b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:35:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'L&#39;Informe de xarxa proporciona informació de rendiment de xarxa en totes les interfícies de xarxa.';
+$lang['network_report_app_name'] = 'Informe de la xarxa';
