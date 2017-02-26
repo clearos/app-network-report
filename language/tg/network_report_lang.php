0ea336fa75dd5826187bc475ccf996896f414cce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:44:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Гузориши Шабакаи иттилоотии throughput дар ҳамаи интерфейсҳои шабакаи шумо пешкаш мекунад.';
+$lang['network_report_app_name'] = 'Њисобот оид ба шабакаи';
