3b8c9d5748b2e4cb18efb1f342f8940addb810a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:39:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'ບົດລາຍງານການເຄືອຂ່າຍສະຫນອງການເຄືອຂ່າຍຂໍ້ມູນຂ່າວສານຜ່ານທັງຫມົດທີ່ເຊື່ອມຕໍ່ເຄືອຂ່າຍຂອງທ່ານ.';
+$lang['network_report_app_name'] = 'Report ເຄືອຂ່າຍ';
