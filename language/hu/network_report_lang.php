aa1d2be2234b66fa8f7e9df96f8d0b7345f7ec72
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:37:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'A Hálózat jelentés biztosítja a hálózati teljesítményt információt az összes hálózati interfész.';
+$lang['network_report_app_name'] = 'hálózati jelentés';
