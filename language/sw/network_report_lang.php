aaeb72c22fc8ee2f3a5e7fabfc77a3762407e8a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:43:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Ripoti Network hutoa mtandao throughput taarifa juu ya interfaces yako yote ya mtandao.';
+$lang['network_report_app_name'] = 'Ripoti Network';
