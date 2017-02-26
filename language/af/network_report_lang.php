b893596eef289477e922dc1bae9a7bbbabd56d2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:33:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Die Netwerk Rapporteer bied netwerk deurset inligting oor al jou netwerk interfaces.';
+$lang['network_report_app_name'] = 'netwerk Verslag';
