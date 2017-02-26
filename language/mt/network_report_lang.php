21a26d8c7499548e9512eeae8c1ae1016f5075dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 18:41:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/network_report_lang.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['network_report_app_description'] = 'Ir-Rapport Network jipprovdi netwerk ta &#39;informazzjoni throughput fuq kull interfaces tan-netwerk tiegħek.';
+$lang['network_report_app_name'] = 'Rapport netwerk';
