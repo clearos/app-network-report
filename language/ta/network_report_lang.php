e462e4b022400bd841462c7c7c309c7234c30d35
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 07:10:30 +0000

    Add new files and directories, commit changes, push changes

--- language/ta/network_report_lang.php
+++ language/ta/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'நெட்வொர்க் அறிக்கை அனைத்து உங்கள் பிணைய முகப்புகளில் பிணையத்தை தகவல்களை வழங்குகிறது.';
-$lang['network_report_app_name'] = 'நெட்வொர்க் அறிக்கை';
+$lang['network_report_app_description'] = 'நெட்வொர்க் அறிக்கை உங்கள் அனைத்து பிணைய இடைமுகங்களுக்கும் நெட்வொர்க் வெளியீடு தகவல் வழங்குகிறது.';
+$lang['network_report_app_name'] = 'பிணைய அறிக்கை';
