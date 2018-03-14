601782f2808ab21d89e3b33a1fd1a4ae07444029
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 07:17:58 +0000

    Add new files and directories, commit changes, push changes

--- language/yi/network_report_lang.php
+++ language/yi/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'די נעץ באריכט גיט נעץ טרופּוט אינפֿאָרמאַציע אויף אַלע אייער נעץ Interfaces.';
-$lang['network_report_app_name'] = 'נעטוואָרק באריכט';
+$lang['network_report_app_description'] = 'די נעטוואָרק באריכט גיט נעץ טראַנזפּוט אינפֿאָרמאַציע אויף אַלע אייער נעץ ינטערפייסיז.';
+$lang['network_report_app_name'] = 'Network Report';
