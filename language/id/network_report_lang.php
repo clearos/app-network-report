8c87842241a9d9270a22471473ffaba9b865bb3a
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:42:48 +0000

    Add new files and directories, commit changes, push changes

--- language/id/network_report_lang.php
+++ language/id/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'Jaringan Report memberikan informasi jaringan throughput pada semua antarmuka jaringan Anda.';
+$lang['network_report_app_description'] = 'Laporan Jaringan menyediakan informasi throughput jaringan pada semua antarmuka jaringan Anda.';
 $lang['network_report_app_name'] = 'Laporan jaringan';
