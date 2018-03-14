f04c62a6e90a901d65d286e29a5ccb69a811aae0
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:44:08 +0000

    Add new files and directories, commit changes, push changes

--- language/ig/network_report_lang.php
+++ language/ig/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'The Network Report na-enye netwọk throughput ozi nile netwọk gị ihu.';
-$lang['network_report_app_name'] = 'Network Report';
+$lang['network_report_app_description'] = 'Network Report na-enye ozi ntanetị netwọk na netwọk netwọk gị niile.';
+$lang['network_report_app_name'] = 'Akaụntụ Network';
