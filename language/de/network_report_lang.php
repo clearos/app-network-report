d7222c5dd01784f445c2f8dab35d27acdc180b01
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:38:24 +0000

    Add new files and directories, commit changes, push changes

--- language/de/network_report_lang.php
+++ language/de/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'Der Netzwerkbericht bietet Netzwerk-Durchsatz Informationen über alle Netzwerk-Schnittstellen.';
+$lang['network_report_app_description'] = 'Der Netzwerkbericht bietet Netzwerkdurchsatzinformationen für alle Ihre Netzwerkschnittstellen.';
 $lang['network_report_app_name'] = 'Netzwerkbericht';
