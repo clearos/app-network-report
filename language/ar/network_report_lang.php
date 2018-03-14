d084c6adbcdd3165e622e087bae3465dd960e162
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:25:22 +0000

    Add new files and directories, commit changes, push changes

--- language/ar/network_report_lang.php
+++ language/ar/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'يوفر تقرير الشبكة شبكة المعلومات الإنتاجية على كافة الواجهات الشبكة.';
+$lang['network_report_app_description'] = 'يوفر "تقرير الشبكة" معلومات سرعة الشبكة على كافة واجهات الشبكة الخاصة بك.';
 $lang['network_report_app_name'] = 'تقرير الشبكة';
