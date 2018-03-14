2db952f1b3bc5adb8614c3462320514bcdc944e2
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 07:16:39 +0000

    Add new files and directories, commit changes, push changes

--- language/vi/network_report_lang.php
+++ language/vi/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'Báo cáo mạng cung cấp mạng thông tin thông trên tất cả các giao diện mạng của bạn.';
-$lang['network_report_app_name'] = 'báo cáo mạng';
+$lang['network_report_app_description'] = 'Báo cáo Mạng cung cấp thông tin thông lượng mạng trên tất cả các giao diện mạng của bạn.';
+$lang['network_report_app_name'] = 'Báo cáo Mạng';
