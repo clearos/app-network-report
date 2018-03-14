4489d34db0c0b215b23f7710a58f3c44b1115e05
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 07:14:01 +0000

    Add new files and directories, commit changes, push changes

--- language/tr/network_report_lang.php
+++ language/tr/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'Ağ Raporu tüm ağ arayüzleri ağ verim bilgiler verilmektedir.';
-$lang['network_report_app_name'] = 'ağ Raporu';
+$lang['network_report_app_description'] = 'Ağ Raporu, tüm ağ arayüzleriniz üzerinde ağ üretim bilgisi sağlar.';
+$lang['network_report_app_name'] = 'Ağ raporu';
