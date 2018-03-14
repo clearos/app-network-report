df1d80d81a8a2cdcd4ea574429fc2316d60fc5c8
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:37:33 +0000

    Add new files and directories, commit changes, push changes

--- language/gl/network_report_lang.php
+++ language/gl/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'O Informe de rede ofrece rede de información taxa de transferencia en todas as súas interfaces de rede.';
+$lang['network_report_app_description'] = 'O informe de rede fornece información de rendemento de rede en todas as interfaces de rede.';
 $lang['network_report_app_name'] = 'Informe de rede';
