b46e03085f98eb8b559f50fa53f911e7eeb3d084
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 07:00:49 +0000

    Add new files and directories, commit changes, push changes

--- language/fa/network_report_lang.php
+++ language/fa/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'گزارش شبکه اطلاعات شبکه توان در تمامی شبکه های خود را فراهم می کند.';
+$lang['network_report_app_description'] = 'گزارش شبکه، اطلاعات شبکه را در تمام واسط های شبکه شما فراهم می کند.';
 $lang['network_report_app_name'] = 'گزارش شبکه';
