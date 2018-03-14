cce4b5650e413cbd945da6b10de349c16e4162fd
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:43:42 +0000

    Add new files and directories, commit changes, push changes

--- language/ga/network_report_lang.php
+++ language/ga/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'Tugann an Tuarascáil Líonra gréasáin faisnéise tréchur ar gach do comhéadan líonra.';
-$lang['network_report_app_name'] = 'Tuarascáil líonra';
+$lang['network_report_app_description'] = 'Soláthraíonn an Tuarascáil Líonra faisnéis tríchur ar an ngréasán ar gach comhéadan líonra atá agat.';
+$lang['network_report_app_name'] = 'Tuarascáil Líonra';
