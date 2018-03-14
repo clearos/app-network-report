507a8809541cbde100aa116caeabdcd4637aa483
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:41:31 +0000

    Add new files and directories, commit changes, push changes

--- language/hi/network_report_lang.php
+++ language/hi/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'नेटवर्क रिपोर्ट अपने सभी नेटवर्क इंटरफेस पर नेटवर्क throughput जानकारी प्रदान करता है।';
-$lang['network_report_app_name'] = 'नेटवर्क की रिपोर्ट';
+$lang['network_report_app_description'] = 'नेटवर्क रिपोर्ट आपके सभी नेटवर्क इंटरफेस पर नेटवर्क थ्रूपुट जानकारी प्रदान करता है।';
+$lang['network_report_app_name'] = 'नेटवर्क रिपोर्ट';
