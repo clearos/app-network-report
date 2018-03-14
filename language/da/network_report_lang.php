d3f26264c921903a72bfc911e6f99299b7405eb9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:34:32 +0000

    Add new files and directories, commit changes, push changes

--- language/da/network_report_lang.php
+++ language/da/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'Netværket rapporten giver netværkshastighed oplysninger om alle dine netværksgrænseflader.';
-$lang['network_report_app_name'] = 'Netværk rapport';
+$lang['network_report_app_description'] = 'Netværksrapporten indeholder oplysninger om netværkets gennemløb på alle dine netværksgrænseflader.';
+$lang['network_report_app_name'] = 'Netværksrapport';
