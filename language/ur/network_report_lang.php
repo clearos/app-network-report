181cad0dd02b27373279e462705204bea4f61c4f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 07:15:21 +0000

    Add new files and directories, commit changes, push changes

--- language/ur/network_report_lang.php
+++ language/ur/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'نیٹ ورک کی رپورٹ آپ کے تمام نیٹ ورک انٹرفیس نیٹ ورک throughput کی معلومات فراہم کرتا ہے.';
+$lang['network_report_app_description'] = 'نیٹ ورک کی رپورٹ آپ کے تمام نیٹ ورک کے انٹرفیس پر نیٹ ورک کے ذریعہ معلومات فراہم کرتا ہے.';
 $lang['network_report_app_name'] = 'نیٹ ورک کی رپورٹ';
