67fcc568e056cb1adc88dacdf96245157c0707b3
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:54:14 +0000

    Add new files and directories, commit changes, push changes

--- language/mk/network_report_lang.php
+++ language/mk/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'Извештајот на мрежата им овозможува на мрежата на автопат информации за сите ваши мрежни интерфејси.';
-$lang['network_report_app_name'] = 'Пријави мрежа';
+$lang['network_report_app_description'] = 'Извештајот Мрежа обезбедува информации за пропусната моќ на сите ваши мрежни интерфејси.';
+$lang['network_report_app_name'] = 'Мрежен извештај';
