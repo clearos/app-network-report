9314b5d453d39e6f758effd4b219fe465711b8b9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-14 06:45:30 +0000

    Add new files and directories, commit changes, push changes

--- language/it/network_report_lang.php
+++ language/it/network_report_lang.php
@@ -1,4 +1,4 @@
 <?php
 
-$lang['network_report_app_description'] = 'Il rapporto di rete fornisce informazioni throughput di rete su tutte le interfacce di rete.';
-$lang['network_report_app_name'] = 'rapporto di rete';
+$lang['network_report_app_description'] = 'Il rapporto di rete fornisce informazioni sulla velocità di trasmissione su tutte le interfacce di rete.';
+$lang['network_report_app_name'] = 'Rapporto di rete';
